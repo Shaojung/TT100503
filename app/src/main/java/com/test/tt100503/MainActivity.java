e805dbbb13b0faafaa64a8a28603b12ca2bac168
Shaojung <shaojung@gmail.com>
2016-10-05 13:35:02 +0800

    加入 click1

--- app/src/main/java/com/test/tt100503/MainActivity.java
+++ app/src/main/java/com/test/tt100503/MainActivity.java
@@ -2,12 +2,19 @@ package com.test.tt100503;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView2);
+    }
+    public void click1(View v)
+    {
+        tv.setText("123123");
     }
 }
