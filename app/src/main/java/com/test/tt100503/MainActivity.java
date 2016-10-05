0aa34ab308d7cf5bcbd1f4fc4ee785b8a2ec6850
Shaojung <shaojung@gmail.com>
2016-10-05 14:39:01 +0800

    Use ToggleButton

--- app/src/main/java/com/test/tt100503/MainActivity.java
+++ app/src/main/java/com/test/tt100503/MainActivity.java
@@ -4,17 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     Button btn2;
+    ToggleButton tb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.textView2);
-
+        tb = (ToggleButton) findViewById(R.id.toggleButton);
         btn2 = (Button)findViewById(R.id.button2);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked == true)
+                {
+                    tv.setText("已選擇");
+                }
+                else
+                {
+                    tv.setText("取消選擇");
+                }
+            }
+        });
     }
 
     public void click1(View v)
